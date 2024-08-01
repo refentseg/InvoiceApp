756546139cf920d9922164f14d8fbeaa81557066
theuglybwe <refentse.dev@gmail.com>
2024-08-01 11:00:03 +0200

    Minor changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b341388ace6a69f38cb4c23d974faaaa55eb66a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6297a379a2eead55a7140ab10c8fe11b3566720")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
